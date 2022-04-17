8cdcd93f308fc4863023f665f252ace4b83b734e
Christoffer Kronblad <crisse.kronblad@gmail.com>
2022-04-17 21:47:39 +0200

    It is now possible to add an X or O to the board by specifying the number on the board.

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
 #include <string>
 
-void placeSymbol(int player);
+void placeSymbol(int currentPlayer, char* placement, char* playerSymbols);
 void drawBoard(char* placement);
 
 
 int main()
 {
+	char playerSymbols[2] = {'X', 'O'};
+	bool gameEnded = false;
+	int currentPlayer = 0;
 	char placement[9] = { '1' , '2' , '3' , '4' , '5' , '6' , '7', '8', '9' };
 	drawBoard(placement);
+
+	while (gameEnded == false)
+	{
+		placeSymbol(currentPlayer, placement, playerSymbols);
+		drawBoard(placement);
+
+		currentPlayer == 0 ? currentPlayer = 1 : currentPlayer = 0;
+	}
 }
 
-void placeSymbol(int player) {
-	char symbol;
-	std::cin >> symbol;
+void placeSymbol(int currentPlayer, char* placement, char* playerSymbols) {
+	
+	std::cout << "Player " << currentPlayer+1 << " choose a box to play in: ";
+	
+	int box;
+	std::cin >> box;
+
+	placement[box - 1] = playerSymbols[currentPlayer];
+	system("cls");
 }
 
 
