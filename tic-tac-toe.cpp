7ec8238313b55516fb94ba68e214050c6cbcd3da
Christoffer Kronblad <crisse.kronblad@gmail.com>
2022-04-17 17:44:07 +0200

    Defualt board with numbers to know where you can place a symbol.

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
+#include <string>
+
+void placeSymbol(int player);
+void drawBoard(char* placement);
+
 
 int main()
 {
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "--------------------" << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "--------------------" << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
-	std::cout << "      |      |      " << std::endl;
+	char placement[9] = { '1' , '2' , '3' , '4' , '5' , '6' , '7', '8', '9' };
+	drawBoard(placement);
 }
+
+void placeSymbol(int player) {
+	char symbol;
+	std::cin >> symbol;
+}
+
+
+void drawBoard(char* placement) {
+
+
+	std::cout << "       |       |       " << std::endl;
+	std::cout << "   "<< placement[0] << "   |   "<< placement[1] << "   |   "<< placement[2] << "   " << std::endl;
+	std::cout << "       |       |       " << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "       |       |       " << std::endl;
+	std::cout << "   " << placement[3] << "   |   " << placement[4] << "   |   " << placement[5] << "   " << std::endl;
+	std::cout << "       |       |       " << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "       |       |       " << std::endl;
+	std::cout << "   " << placement[6] << "   |   " << placement[7] << "   |   " << placement[8] << "   " << std::endl;
+	std::cout << "       |       |       " << std::endl;
+}
+
+
